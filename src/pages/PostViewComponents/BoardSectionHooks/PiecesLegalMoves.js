4331d6111fa3d4e1cfbbb9d3d029d2e1539cdc3d
Kosmo00 <vladiotero19101916@gmail.com>
2021-05-03 01:48:09 -0400

    refactoring and fixing bugs

--- src/pages/PostViewComponents/BoardSectionHooks/PiecesLegalMoves.js
+++ src/pages/PostViewComponents/BoardSectionHooks/PiecesLegalMoves.js
@@ -4,8 +4,10 @@ const legal_movements = {
       let { selected_piece, pieces_colocation, legal_moves } = state
       const color_piece = getColorOfPiece(selected_piece, pieces_colocation)
       let direction = 1
+      let initial_position = 1
       if (color_piece === 'W') {
         direction = -1
+        initial_position = 6
       }
       if (isInBoard(selected_piece, 1 * direction, 1)) {
         const square_color_piece = getColorOfPiece(selected_piece, pieces_colocation, 1 * direction, 1)
@@ -16,7 +18,6 @@ const legal_movements = {
       if (isInBoard(selected_piece, 1 * direction, -1)) {
         const square_color_piece = getColorOfPiece(selected_piece, pieces_colocation, 1 * direction, -1)
         if (square_color_piece !== color_piece && square_color_piece !== undefined) {
-          console.log(square_color_piece)
           legal_moves[selected_piece[0] + 1 * direction][selected_piece[1] - 1] = true
         }
       }
@@ -24,7 +25,7 @@ const legal_movements = {
       if (square_color_piece === undefined) {
         legal_moves[selected_piece[0] + 1 * direction][selected_piece[1]] = true
         square_color_piece = getColorOfPiece(selected_piece, pieces_colocation, 2 * direction, 0)
-        if (square_color_piece === undefined) {
+        if (square_color_piece === undefined && initial_position === selected_piece[0]) {
           legal_moves[selected_piece[0] + 2 * direction][selected_piece[1]] = true
 
         }
